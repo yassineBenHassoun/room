4cfbd04affc1a9f7768d58644cd0e3fe5e834c10
BenHassoun <y.benhassoun@gmail.com>
2022-12-22 10:44:33 +0100

    add seeder

--- Application/Spring/src/main/java/com/spring/seeder/UserSeeder.java
+++ Application/Spring/src/main/java/com/spring/seeder/UserSeeder.java
@@ -2,9 +2,15 @@ package com.spring.seeder;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import com.spring.entity.Role;
 import com.spring.entity.User;
 import com.spring.enums.RoleEnum;
+import com.spring.repository.RoleRepository;
 import com.spring.repository.UserRepository;
 
 import org.springframework.stereotype.Component;
@@ -15,6 +21,12 @@ public class UserSeeder implements CommandLineRunner
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    PasswordEncoder encoder;
+
+    @Autowired
+    RoleRepository roleRepository;
+
     @Override
 	public void run(String... args) throws Exception {
 		generateUser();
@@ -24,9 +36,19 @@ public class UserSeeder implements CommandLineRunner
 
         if(userRepository.count() == 0) {
 
-            User u1 = new User("yass","bnh","yasssinebnh","y.benhassoune@gmail.com","susanoo",0661265623);
+            User u1 = new User("yass","bnh","superadmin","y.benhassoune@gmail.com",encoder.encode("susanoo"),0661265623);
 
+            Set<Role> roles = new  HashSet<>();
+            Role userRole = roleRepository.findByName(RoleEnum.USER).orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+            roles.add(userRole);
+            Role adminRole = roleRepository.findByName(RoleEnum.ADMIN).orElseThrow(() -> new RuntimeException("Error: Role Admin is not found."));
+            roles.add(adminRole);
+            Role modRole = roleRepository.findByName(RoleEnum.MODERATOR).orElseThrow(() -> new RuntimeException("Error: Modo is not found."));
+            roles.add(modRole);
+
+            u1.setRoles(roles);
             userRepository.save(u1);
+  
         }
     }
 
